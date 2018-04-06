f27a950db86c563ff63675571758516afb766fba
Lan Ejiri <lan.ejiri@yahoo.com>
2018-04-06 12:25:34 -0400

    meh

--- liri.js
+++ liri.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 
 //STEP 9
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
+
+
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
+//     var results = response.data;
+// });
