3de13c8073dab5ed7c91591932a1e62a3bf7e747
Lan Ejiri <lan.ejiri@yahoo.com>
2018-04-08 17:42:19 -0400

    done with twitter things

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# LIRI-node-app
+# LIRI-node-app HW ASSIGNMENT
+
+## About LIRI
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+## Commands
+
+### Typing `node liri.js` will welcome the user and show all commands
+
+
+1. Twitter
+    * `my-tweets` 
+        * will show 20 most recent tweets by CodingCakez (My test twitter acount)
+    * `my-tweets <twitterhandle>` 
+        * will show 20 most recent tweets by that specified username
+        * if that user does not exist, it will display a error message.
+
+2. Spotify
+
+* `spotify-this-song`
+
+* `movie-this`
+
+* `do-what-it-says`
--- liri.js
+++ liri.js
@@ -1,16 +1,105 @@
 require("dotenv").config();
 
+//require things
+var Twitter = require("twitter");
+
+var spotify = require("node-spotify-api");
+var request = require("request");
+var fs = require("fs");
+
+
 //STEP 9
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var keys = require("./keys.js");
+var client = new Twitter(keys.twitter);
+
+var command = process.argv[2];
+var value = process.argv[3];
+
+// if there is no command, welcome the user
+if (process.argv.length === 2) {
+    console.log("Hi, welcome to LIRI. Please enter one of the following commands:");
+    console.log("my-tweets along with a twitter handle to see that user's 20 most recent tweets. If you do not enter a handle, you will see CodingCakez's 20 most recent tweets.");
+    console.log("spotify-this-song");
+    console.log("movie-this");
+    console.log("do-what-it-says");
+} //closing if there is no command.F
+
+//if there is a command, then do the do
+else {
+
+    //if the command is my tweets
+    if (command === 'my-tweets') {
+
+        //if a twitter handle is not specified, show CodingCakez's tweets
+        if (typeof process.argv[3] === 'undefined') {
+
+            console.log("CodingCakez's 20 most recent tweets");
+            var params = { screen_name: 'CodingCakez' };
+
+            client.get('statuses/user_timeline', params, function (error, tweets, response) {
+                if (!error) {
+                    var j = 20;
+                    for (i = 0; i < tweets.length; i++) {
+                        console.log('=========================================');
+                        console.log('Posted on: ' + tweets[i].created_at);
+                        console.log('" ' + tweets[i].text + ' "');
+
+                        j--;
+                    }
+                }
+            });
+        } //closing if twit handle not specified
+
+        // if a handle is speficied, show that user's tweets
+        else {
+            var params = { screen_name: value };
+
+            client.get('statuses/user_timeline', params, function (error, tweets, response) {
+
+                if (!error) {
+                    var j = 20;
+                    console.log(value + "'s 20 most recent tweets");
+
+                    for (i = 0; i < tweets.length; i++) {
+
+                        console.log('=========================================');
+                        console.log('Posted on: ' + tweets[i].created_at);
+                        console.log('" ' + tweets[i].text + ' "');
+
+                        j--;
+                    }
+                }
+                else {
+                    console.log("sorry, that username does not exist")
+                }
 
 
+            });
+        } //closing if a twit handle specified
+    } //closing if my-tweets
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function (response) {
-//     console.log(response);
-//     var results = response.data;
-// });
+    //if user types spotyisgdohsjdfkkg
+    else if (command === 'spotify-this-song') {
+        console.log("spotify this song")
+    } //closing else if spotify
+
+    //if user types movie this
+    else if (command === 'movie-this') {
+        console.log("movie-this");
+    } //closing else if movie this
+
+    //if user types do what it says
+    else if (command === 'do-what-it-says') {
+        console.log("do-what-it-says");
+    } //closing elseif do what it says
+    
+    //if user types something funky
+    else {
+        console.log("Sorry, that is not a valid LIRI command. Please enter one of the following:");
+        console.log("my-tweets");
+        console.log("spotify-this-song");
+        console.log("movie-this");
+        console.log("do-what-it-says");
+    }//closing if user types invalid command
+
+} //closing if there is a command
